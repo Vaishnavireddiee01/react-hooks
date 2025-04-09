035f11bf804e5176d49f6b269d52d14014b38e09
Vaishnavireddiee01 <vanugu47@gmail.com>
2025-04-09 19:44:23 +0530

    usestate with prev.state

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import React from 'react';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
 
+import HookCounterTwo from './components/HookCounterTwo';
+import ClassCounterTwo from './components/ClassCounterTwo';
 import './App.css';
-
 function App() {
   return (
     <div className="App">
-      <h1>class counter</h1>
+      <h3>class counter</h3>
       <ClassCounter />
-      <h1>Hook counter</h1>
+      <h3>Hook counter</h3>
       <HookCounter />
+      <h4>HookCounterTwo</h4>
+      <HookCounterTwo />
+      <h4>ClassCounterTwo</h4>
+      <ClassCounterTwo />
     </div>
   );
 }
